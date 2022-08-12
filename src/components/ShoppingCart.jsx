8c19085dea7947177194ed1cc94828eb78049a29
Marshnme <jildsmiles@yahoo.com>
2022-08-12 00:58:38 -0400

    displaying total of all items in cart

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,6 +1,29 @@
 import './styleSheets/ShoppingCart.css'
-
+import { useState,useEffect} from 'react'
 const ShoppingCart = (props) => {
+    const [total, setTotal] = useState(0)
+
+    useEffect(() => {
+        findTotal()
+    },[props.addQuantity,props.minusQuantity])
+    
+    function findTotal() {
+        let newTotal = 0;
+        for (let i = 0; i < props.cart.items.length; i++){
+            if (props.cart.items[i].quantity > 1) {
+                for (let j = 0; j < props.cart.items[i].quantity; j++){
+                    console.log('multiple run')
+                    newTotal += props.cart.items[i].item.price
+                }
+            } else {
+                 console.log('single run')
+                newTotal += props.cart.items[i].item.price
+            }
+           
+        }
+
+        setTotal(newTotal.toFixed(2))
+    }
     return (
         <main className='shopping-cart-parent'>
             <h1>ShoppingCart</h1>
@@ -8,14 +31,21 @@ const ShoppingCart = (props) => {
             {props.cart.items.map((item) => {
                 console.log(item)
                 return (
-                    <div key={item.item.id}>
+                    <div className='cart-item' key={item.item.id}>
                         <p><span>${item.item.price} - </span>{item.item.title}</p>
-                        <p><span onClick={() => { props.minusQuantity(item) }}>minus</span>{item.quantity}<span onClick={() => { props.addQuantity(item) }}>add</span></p>
+                        <div className='quantity-parent'>
+                            <p className='minus-quantity' onClick={() => { props.minusQuantity(item) }}>-</p>
+                            <p>{item.quantity}</p>
+                            <p className='add-quantity'onClick={() => { props.addQuantity(item) }}>+</p>
+                        </div>
+                        
 
                     </div>
                     
                 )
             })}
+
+            <p>${total}</p>
         </main>
     )
 }
